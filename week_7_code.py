d88959ecc1031201d915009f6071b6cde891a300
Modesto Cabrera <cabreramod@gmail.com>
2015-10-12 11:12:51 -0400

    code
    code

--- week_7_code.py
+++ week_7_code.py
@@ -1,19 +1,24 @@
-import pprint
+#!usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Reads HTML Table and returns a Tuple"""
 
 import pprint
-import sys, urllib2, argparse, re
-
+import urllib2, argparse, re
 from bs4 import BeautifulSoup
 
 
-#  &&&&&&&&& READ CONTENT OF HTML table from a web page into pandas DATAFRAME&&&&&&&&&
-
-
-
-#this is where the link is read
-
-
 def url_download(url):
+    """
+    Args:
+        url_file = request to open url link.
+
+    Returns:
+        url_file = file access conenction to url.
+
+    Examples:
+        >>> url_download('http://www.google.com'
+        >>>
+    """
     try:
         url_file = urllib2.urlopen(url)
 
@@ -25,30 +30,34 @@ def url_download(url):
 
 
 def parse_site(url):
-    column1 = []
-    column2 = []
-    column3 = []
-    
-    soup_obj = BeautifulSoup(url, 'html.parser')
-    table =  soup_obj.find('table')
-    for row in  table.find_all('tr'):
-        cell = row.find_all('td')
-        database += [cell[0].text, cell[1].text, cell[2].text]
-    print database
+    """
+    Args:
+        data (dict): dictionary holds index keys, and values of data.
+        title (list): list holds title of columns of table.
 
+    Returns:
+        output (tup): Tuple of title list and data dictionary.
 
-#Some of the rows where not catching, last night using dicts and values. list may not work.
-#NOT CATCHING ALL THE ROWS, MUST USE REGEX...
-#            try:                
-#                database[cells[0].text] = [float(cells[1].text), float(cells[2].text)]
-#            except:
-#                print cells[0].text, cells[1].text, cells[2].text
-#                print 'error here'
-#                titles = [cells[0].text, cells[1].text, cells[2].text]
-#        pprint.pprint(database)    
-###ahhhh i am not done yet, but i will upload it, and finish this tomorow.         
-    
-    
+    Examples:
+        >>> parse_site(url)
+        >>> 
+    """
+    data = {}
+    title = []
+    try:
+        soup_obj = BeautifulSoup(url, 'html.parser')
+        table =  soup_obj.find('table')
+        for row in  table.find_all('tr'):
+            cell = row.find_all('td')
+            if re.search(r"[0-9]", cell[0].text):
+                data[cell[0].text] = [cell[1].text, cell[2].text]
+            else:
+                title = cell[0].text, cell[1].text, cell[2].text
+        output = (title, data)
+        return output
+    except ValueError:
+        print 'ValueError Please check URL format'
+        raise
 
 
 def main():
